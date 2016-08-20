6966050062f87b25244c3f0580b296b592eca4c2
Mohsin Ali <mohammadmohsin@gmail.com>
2016-08-20 21:24:08 +0500

    Hell yeah.... Routing done the right way..

--- app/categories/categories.module.ts
+++ app/categories/categories.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 
 import { CategoriesComponent } from './categories.component'
+import { categoriesRouting } from './categories.routing'
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, categoriesRouting],
   declarations: [CategoriesComponent],
   providers: []
 })
